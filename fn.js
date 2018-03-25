edf9a41dad110772d05c9cad02acca343b1efd8f
Javier <jflores7077@baysidehighschool.org>
2018-03-25 18:05:15 -0400

    Fixes/fn file

---
+++ fn.js
@@ -0,0 +1,181 @@
+//Render map
+function renderMap(x,y) {
+
+    //Coordinates
+    var latlng = new google.maps.LatLng(x, y);
+    
+    var map=new google.maps.Map(document.getElementById('map'), {
+        center: latlng,
+        zoom: 18,
+    });
+
+    google.maps.event.trigger(map, 'resize');
+}
+
+function loadedData(){
+    console.log('LOADED DATA')
+}
+
+//Get data and load it into gData
+function getData(){
+    $.getJSON( 'https://data.cityofnewyork.us/resource/byk8-bdfw.json', function( data ) {
+        gData = data
+        console.log(gData)
+        loadedData();
+    });
+}
+
+//Build html from gData
+function buildData(data,d){
+    
+    var template = document.getElementById('mainTempl').innerHTML
+    var build = '';
+    //Hold number of results
+    var resultNum = 0;
+
+    if(!d){
+        //Search query
+        var searchFor = $('#input1').val()
+        //display search query to user
+        $('#sQuery').text(searchFor)
+        //Split user's response into array, slice @ ,
+        searchFor = searchFor.split(",");
+        //Loop through the array and get rid of any spaces
+        for(var i in searchFor){
+            searchFor[i] = searchFor[i].replace(/ /g, "");
+        }
+
+    }else{
+        //Search query
+        var searchFor = $('#input2').val()
+        //display search query to user
+        $('#sQuery').text(searchFor)
+        
+    }
+    
+    
+    
+    //Loop through data 
+    for(var i in data){
+        //Loop through user search query array
+        for(var j = 0; j<searchFor.length;j++){
+            //Convert to lowercase and remove spaces
+            var newgData = data[i]['borough'].toLowerCase().replace(/ /g, "")
+            if(!d){
+                var newgUser = searchFor[j].toLowerCase().replace(/ /g, "")
+            }else{
+                var newgUser = searchFor.toLowerCase().replace(/ /g, "")
+            }
+
+            //If they're equal, add one to result num and add to build
+            if(newgData == newgUser){
+                resultNum+=1;
+                build += Mustache.render(template, data[i])
+                
+            }
+        }
+    }
+    
+    //Display results
+    $('#sResults').text(resultNum)
+    $('#dataReturn').html(build);
+}
+
+function removeOverlay(){
+    $('#loading').css('pointer-events',' none')
+    $('#loading').addClass('fadeOut')
+}
+
+function goSearch(){
+    $('#map').animate({
+        'left':'100vw'
+    },400,function(){
+        $('#Madeby').animate({
+            'opacity':'1'
+        },200)
+        $('#head').animate({
+            'height':'100vh'
+        },200)
+    });
+}
+
+function searchBtn(d){
+    //Prevent animation from toggling
+    $('#search input').addClass('addAnim');
+
+    if($('#input1').val()=='' && !d){
+        //Prevent search on empty query
+        $('#input1').css({
+            'background':'rgba(219, 57, 8, 0.9)',
+            'transform':'rotate(2deg)'
+        })
+        setTimeout(function(){
+            $('#input1').css({
+                'background':'rgba(219, 57, 8, 0)',
+                'transform':'rotate(0deg)'
+            })
+        }, 300);
+    }else{
+        $('#map').animate({
+            'left':'0'
+        },400,function(){buildData(gData,d)});
+        //$('body').css('overflow-x','scroll')
+        $('#dataReturn').text('')
+        $('#Madeby').animate({
+            'opacity':'0'
+        },200)
+        $('#head').animate({
+            'height':'45vh'
+        },200)
+        
+    }
+    
+}
+
+function switchHead(dropboxb,dText){
+    if(!dropbox){
+        dropbox =  true;
+        dText = 'drop down menu';
+
+        $('#method').animate({
+            'width':'-100vw'
+        },300,function(){
+            $('#method').html(dText)
+            $('#method').animate({
+                'width':'100%'
+            },100);
+        });
+
+        $('#input1').animate({
+            'width':'0vw'
+        },200,function(){
+            
+            $('#input2').animate({
+                'width':'10vw',
+                'display':'inline'
+            },200);
+        });
+
+    }else{
+        dropbox =  false;
+        dText = 'text';
+
+        $('#method').animate({
+            'width':'-100vw'
+        },300,function(){
+            $('#method').html(dText)
+            $('#method').animate({
+                'width':'100%'
+            },100);
+        });
+
+        $('#input2').animate({
+            'width':'0vw'
+        },200,function(){
+            $('#input1').animate({
+                'width':'10vw',
+                'display':'inline'
+            },200);
+        });
+    }   
+}
--- main.js
+++ main.js
@@ -2,176 +2,50 @@
 - Your work must include JavaScript with Mustache, jQuery, HTML and CSS.
 - You must display a stand-alone Google Map (within a container) using location information.
 - Extra credit for teams that use features such as Bootstrap (a framework for responsive web design)
+---Javier F.
 */
+
+//Load data into gData to store locally
 var gData = {};
 var finishedLoading = false;
 
-function renderMap(x,y) {
-    var latlng = new google.maps.LatLng(x, y);
-    
-    var map=new google.maps.Map(document.getElementById('map'), {
-        center: latlng,
-        zoom: 18,
-    });
-
-    console.log('map')
-    google.maps.event.trigger(map, 'resize');
-
-    /** 
-    latLong = new google.maps.LatLng(47.0303105, 28.815481);
-    map = new google.maps.Map(document.getElementById('canvas'), {
-        center: latLong,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 14
-    });
-    console.log("Map is ready.")
-    */
-}
-
-function loadedData(){
-    console.log('LOADED DATA')
-}
-
-function getData(){
-    $.getJSON( 'https://data.cityofnewyork.us/resource/byk8-bdfw.json', function( data ) {
-        gData = data
-        console.log(gData)
-        loadedData();
-    });
-}
-function buildData(data){
-    
-    var template = document.getElementById('mainTempl').innerHTML
-    var build = '';
-    var resultNum = 0;
-    var searchFor = $('#input1').val()
-    $('#sQuery').text(searchFor)
-    searchFor = searchFor.split(",");
-
-    for(var i in searchFor){
-        searchFor[i] = searchFor[i].replace(' ', '');
-    }
-    
-    
-
-    for(var i in data){
-        for(var j = 0; j<searchFor.length;j++){
-            console.log(searchFor[j])
-            if(data[i]['borough'].toLowerCase().replace(' ', '') == searchFor[j].toLowerCase().replace(' ', '')){
-                resultNum+=1;
-                build += Mustache.render(template, data[i])
-                
-            }
-
-        }
-        
-        
-    }
-    
-    $('#sResults').text(resultNum)
-    $('#dataReturn').html(build);
-}
 var dropbox = false;
 var dText = ' text'
 
-function switchHead(){
-    if(!dropbox){
-        dropbox =  true;
-        dText = 'drop down menu';
-
-        $('#method').animate({
-            'width':'-100vw'
-        },300,function(){
-            $('#method').html(dText)
-            $('#method').animate({
-                'width':'100%'
-            },100);
-        });
-
-        $('#input1').animate({
-            'width':'0vw'
-        },200,function(){
-            
-            $('#input2').animate({
-                'width':'10vw',
-                'display':'inline'
-            },200);
-        });
-
-    }else{
-        dropbox =  false;
-        dText = 'text';
-
-        $('#method').animate({
-            'width':'-100vw'
-        },300,function(){
-            $('#method').html(dText)
-            $('#method').animate({
-                'width':'100%'
-            },100);
-        });
-
-        $('#input2').animate({
-            'width':'0vw'
-        },200,function(){
-            $('#input1').animate({
-                'width':'10vw',
-                'display':'inline'
-            },200);
-        });
-
-       
-    }
-
-    
-    
-}
-
+//Load data
 getData();
 
 $(document).ready(function(){
-    $('#loading').css('pointer-events',' none')
-    $('#loading').addClass('fadeOut')
     $(this).scrollTop(0);
+    removeOverlay();
 
-    $('#title').click(switchHead)
+    //on click run switchHead
+    $('#title').click(function(){
+        switchHead(dropbox,dText)
+    })
 
-    $('#info_search').click(function(){
-        $('#map').animate({
-            'left':'100vw'
-        },400,function(){
-            $('#head').animate({
-                'height':'100vh'
-            },200)
-        });
-    });
-    document.getElementById('btn').onclick = function(){
-        $('#search input').addClass('addAnim');
-        if($('#input1').val()==''){
-            $('#input1').css({
-                'background':'rgba(219, 57, 8, 0.9)',
-                'transform':'rotate(2deg)'
-            })
-            setTimeout(function(){
-                $('#input1').css({
-                    'background':'rgba(219, 57, 8, 0)',
-                    'transform':'rotate(0deg)'
-                })
-            }, 500);
-        }else{
-            $('#map').animate({
-                'left':'0'
-            },400,function(){buildData(gData)});
-            //$('body').css('overflow-x','scroll')
-            $('#dataReturn').text('')
-            
-            $('#head').animate({
-                'height':'45vh'
-            },200)
+    //Click back on map to go back to landing
+    $('#info_search').click(goSearch);
+
+    //Clickon search button
+    $('#btn').click(function(){
+        searchBtn(dropbox)
+        $('#input1').blur();
+    })
+    //Press enter to search
+    $('body').on('keyup', '#input1', function (event) {
+        if(!dropbox){
+            //prevent default action
+            event.preventDefault();
+            //Enter runs dropbox
+            if (event.keyCode === 13) {
+                searchBtn(dropbox)
+                $('#input1').blur();
+            }
         }
-        
-    }
+    });
 
+    //On hover of headCont
     $('#headCont').hover(function(){
         $('#search input').toggleClass('addAnim');
         $('#search input').toggleClass('removeAnim');
@@ -182,7 +56,6 @@ $(document).ready(function(){
         var panel = $(this).parent(); 
         var lat = $(panel).find('.lat').text()
         var lng = $(panel).find('.lng').text()
-        console.log(lat)
         renderMap(lat,lng)
     });
 });
